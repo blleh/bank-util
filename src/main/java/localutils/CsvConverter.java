1742bbfaccd5fa5f98683097cca1ae92a34c40d6
blleh <tomasz.dubikowski@gmail.com>
2025-01-20 11:44:21 +0100

    cleanup

--- src/main/java/localutils/CsvConverter.java
+++ src/main/java/localutils/CsvConverter.java
@@ -1,4 +1,4 @@
-package org.example;
+package localutils;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
--- src/main/java/localutils/FileMapper.java
+++ src/main/java/localutils/FileMapper.java
@@ -1,4 +1,4 @@
-package org.example;
+package localutils;
 
 import java.io.IOException;
 import java.nio.file.Files;
